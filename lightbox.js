0835178c2559e3eea3b2f7b45ef03670e42009cd
mvattumilli198 <mvattumilli@gmail.com>
2025-11-27 22:50:49 -0600

    Update lightbox.js

--- lightbox.js
+++ lightbox.js
@@ -1,19 +1,1 @@
-// Select all images inside .image-card
-const images = document.querySelectorAll('.image-card img');
-const lightbox = document.getElementById('lightbox');
-const lightboxImg = document.getElementById('lightbox-img');
 
-// Open lightbox
-images.forEach(img => {
-  img.addEventListener('click', () => {
-    lightbox.classList.add('active');
-    lightboxImg.src = img.src;
-  });
-});
-
-// Close when clicking outside image
-lightbox.addEventListener('click', (e) => {
-  if (e.target !== lightboxImg) {
-    lightbox.classList.remove('active');
-  }
-});
